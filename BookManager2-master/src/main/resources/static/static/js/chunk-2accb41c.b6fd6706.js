9dd7df79996d9b4377659ae591c1c05adad0efa4
zkx <123>
2025-06-17 12:24:59 +0000

    234

--- BookManager2-master/src/main/resources/static/static/js/chunk-2accb41c.b6fd6706.js
+++ BookManager2-master/src/main/resources/static/static/js/chunk-2accb41c.b6fd6706.js
@@ -30,7 +30,7 @@
                         staticClass: "title-container"
                     }, [e("h3", {
                         staticClass: "title"
-                    }, [t._v("凌志大厦图书管理系统-登录界面")])]), e("el-form-item", {
+                    }, [t._v("凌志LKG图书管理系统-登录界面_ZKX")])]), e("el-form-item", {
                         attrs: {
                             prop: "username"
                         }
